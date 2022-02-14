17b9e74e5087fe686a872fcb109f20a0aef042f7
Thrifty <thriftygsf@gmail.com>
2022-02-15 00:00:01 +0530

    changes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,9 +4,9 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <div className="navbar">
-        <h3>Logo</h3>
+      <h3>Axiamatic</h3>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
